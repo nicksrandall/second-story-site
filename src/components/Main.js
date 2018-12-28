edc9ee583a74e71ed75c525296fb51837d1297d2
Nick Randall <nicksrandall@gmail.com>
2018-12-27 22:56:01 -0700

    add new title, services, and rates

--- src/components/Main.js
+++ src/components/Main.js
@@ -31,7 +31,7 @@ class Main extends React.Component {
         >
           <h2 className="major">Intro</h2>
           <p>
-            Lisa Randall is a Licensed Master Social Worker who enjoys
+            Lisa Randall is a Licensed Clinical Social Worker who enjoys
             connecting with her clients and helping them to see themselves from
             a new perspective. She sees clients with a large range of
             challenges, including depression, anxiety, and addiction. She
@@ -40,7 +40,7 @@ class Main extends React.Component {
             and unity within the family.
           </p>
           <p>
-            Lisa has a keen and genuine interest in people and their ‘’stories.”
+            Lisa has a keen and genuine interest in people and their "stories.”
             She believes that within each person lies a powerful, resilient
             spirit regardless of life’s circumstances. She helps clients to see
             that self- awareness and self-compassion are important elements in
@@ -82,7 +82,16 @@ class Main extends React.Component {
           <span className="image main">
             <img src={pic02} alt="" />
           </span>
-          <h3 className="minor">EMDR</h3>
+          <h3 className="minor">EFT for Couples</h3>
+          <p>
+            As an emotionally focused couples therapist, Lisa’s approach is
+            rooted in attachment theory. She will assist her clients to become
+            fully aware of unrecognized, unshared, connective primary emotions
+            and reactive, disconnecting secondary emotions. She helps couples
+            cultivate greater awareness and acceptance of emotion and also
+            teaches each partner to express greater empathy for each other.
+          </p>
+          <h3 className="minor">EMDR for Trauma</h3>
           <p>
             EMDR (Eye Movement Desensitization and Reprocessing) is a
             psychotherapy that enables people to heal from the symptoms and
@@ -91,19 +100,12 @@ class Main extends React.Component {
             can experience the benefits of psychotherapy that once took years to
             make a difference.
           </p>
-          <h3 className="minor">DBT</h3>
+          <h3 className="minor">DBT for Emotion Regulation</h3>
           <p>
             Dialectical behavior therapy provides clients with new skills to
             manage painful emotions and decrease conflict in relationships.
           </p>
-          <h3 className="minor">Shame identification and resiliency</h3>
-          <p>
-            Shame is the underlying mechanism that brings most people to
-            therapy. It may be disguised as anxiety or depression but is the
-            root cause of most issues.
-          </p>
           <Appt />
-          {close}
           {close}
         </article>
 
@@ -114,6 +116,38 @@ class Main extends React.Component {
           }`}
           style={{ display: 'none' }}
         >
+          <h2 className="major">Rates & Insurance</h2>
+          <h3 className="minor">Rate</h3>
+          <p>
+            $125 for a 50 minute session.  Longer sessions are also available
+            and may especially benefit couples.
+          </p>
+          <h3 className="minor">Insurance</h3>
+          <p>
+            Although we are NOT "IN-NETWORK" for any insurance plans, services
+            may be covered in full or in part by your health insurance or
+            employee benefit plan. Please check your coverage carefully by
+            asking the following questions:
+          </p>
+          <ul>
+            <li>
+              Do I have mental health insurance benefits for out of network
+              services?
+            </li>
+            <li>What is my deductible and has it been met?</li>
+            <li>How many sessions per year does my health insurance cover?</li>
+            <li>What is the coverage amount per therapy session?</li>
+            <li>Is approval required from my primary care physician?</li>
+          </ul>
+
+          <h3 className="minor">Payment</h3>
+          <p>Cash, check, debit/credit are accepted for payment.</p>
+          <h3 className="minor">Cancellation Policy</h3>
+          <p>
+            If you do not show up for your scheduled therapy appointment, and
+            you have not notified us at least 24 hours in advance, you will be
+            required to pay a $50 no-show fee.
+          </p>
           {close}
         </article>
 
@@ -129,46 +163,59 @@ class Main extends React.Component {
             <img src={pic03} alt="" />
           </span>
           <Appt />
-          <h3>Address</h3>
-          <p>
-            <a href="https://www.google.com/maps/place/4320+E+Brown+Rd,+Mesa,+AZ+85205">
-              Lisa Randall, LMSW
-              <br />
-              4320 E. Brown Road
-              <br />
-              Mesa, AZ 85205
-            </a>
-          </p>
-          {close}
-          <h3>Phone</h3>
-          <p>
-            <a href="tel:4803715213">(480) 688-8282</a>
-          </p>
-          <h3>Email</h3>
-          <p>
-            <a href="mailto:lisarandalltherapy@gmail.com">
-              LisaRandallTherapy@gmail.com
-            </a>
-          </p>
-          <h3>Social</h3>
-          <ul className="icons">
-            <li>
-              <a
-                href="https://www.facebook.com/lisarandallLMSW"
-                className="icon fa-facebook"
-              >
-                <span className="label">Facebook</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.instagram.com/lisarandalltherapy"
-                className="icon fa-instagram"
-              >
-                <span className="label">Instagram</span>
-              </a>
-            </li>
-          </ul>
+          <div className="content__container">
+            <div className="row">
+              <div className="column">
+                <h3>Address</h3>
+                <p>
+                  <a href="https://www.google.com/maps/place/4320+E+Brown+Rd,+Mesa,+AZ+85205">
+                    Lisa Randall, LCSW
+                    <br />
+                    4320 E. Brown Road
+                    <br />
+                    Mesa, AZ 85205
+                  </a>
+                </p>
+              </div>
+              <div className="column">
+                <h3>Phone</h3>
+                <p>
+                  <a href="tel:4803715213">(480) 688-8282</a>
+                </p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="column">
+                <h3>Email</h3>
+                <p>
+                  <a href="mailto:lisarandalltherapy@gmail.com">
+                    LisaRandallTherapy@gmail.com
+                  </a>
+                </p>
+              </div>
+              <div className="column">
+                <h3>Social</h3>
+                <ul className="icons">
+                  <li>
+                    <a
+                      href="https://www.facebook.com/lisarandallLMSW"
+                      className="icon fa-facebook"
+                    >
+                      <span className="label">Facebook</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://www.instagram.com/lisarandalltherapy"
+                      className="icon fa-instagram"
+                    >
+                      <span className="label">Instagram</span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
           {close}
         </article>
       </div>
